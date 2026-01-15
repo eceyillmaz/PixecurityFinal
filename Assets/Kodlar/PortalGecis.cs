0d7132951abed1e2a822b84798832313b928aef7
eceyilmaz <114501070+eceyillmaz@users.noreply.github.com>
2026-01-15 18:42:39 +0300

    PortalGecis.cs

--- Assets/Kodlar/PortalGecis.cs
+++ Assets/Kodlar/PortalGecis.cs
@@ -4,14 +4,12 @@ using UnityEngine.SceneManagement;
 public class PortalGecis : MonoBehaviour
 {
     [Header("Hedef Sahne Ayarı")]
-    // Buraya hangi sahneye gitmek istiyorsan onun adını yazacaksın
     public string hedefSahneAdi; 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // Boş bırakmadıysan yazılan sahneye git
             if (!string.IsNullOrEmpty(hedefSahneAdi))
             {
                 SceneManager.LoadScene(hedefSahneAdi);
