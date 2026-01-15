4e4e06551f87dbae0d6e88782599dc4cbc86266f
eceyilmaz <114501070+eceyillmaz@users.noreply.github.com>
2026-01-15 18:38:21 +0300

    DosyaTopla.cs

--- Assets/Kodlar/DosyaTopla.cs
+++ Assets/Kodlar/DosyaTopla.cs
@@ -4,15 +4,14 @@ public class DosyaTopla : MonoBehaviour
 {
     
     [Header("Ayarlar")]
-    public int dosyaPuani = 50; // Her dosya toplandığında kaç puan eklensin?
+    public int dosyaPuani = 50; 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Eğer dosyaya çarpan objenin Tag'i "Player" ise
         if (other.CompareTag("Player"))
         {
-            // PuanSistemi scriptindeki "PuanArttir" fonksiyonunu çağırıyoruz
-            // Not: PuanSistemi'nde 'instance' yapısı kullandığımızı varsayıyorum
+            // PuanSistemi scriptindeki "PuanArttir" fonksiyonunu çağır
             if (PuanSistemi.instance != null)
             {
                 PuanSistemi.instance.PuanArttir(dosyaPuani);
